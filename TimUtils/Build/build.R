600347ee3ca4ae4a3b071de00409f13af745e95f
tim <tim@triffe-N80Vm>
2014-08-16 11:15:03 -0700

    .

--- TimUtils/Build/build.R
+++ TimUtils/Build/build.R
@@ -4,3 +4,8 @@ load_all("/home/triffe/git/TimUtils/TimUtils")
 document("/home/triffe/git/TimUtils/TimUtils")
 
 IncrementVersion("/home/triffe/git/TimUtils/TimUtils",major.version="01",package.origin = "2014-02-08")
+
+
+#install.packages("devtools")
+#devtools::install_github("devtools")
+
